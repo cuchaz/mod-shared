e4726f9d7ab6b755d61551ed91bdee11cb111e19
hg <devnull@localhost>
2013-06-16 13:27:42 -0400

    added block utilities

---
+++ src/cuchaz/modsShared/BlockUtils.java
@@ -0,0 +1,120 @@
+package cuchaz.modsShared;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.world.World;
+
+public class BlockUtils
+{
+	public static interface BlockValidator
+	{
+		public boolean isValid( World world, int x, int y, int z );
+	}
+	
+	public static int getManhattanDistance( ChunkCoordinates a, ChunkCoordinates b )
+	{
+		return getManhattanDistance( a.posX, a.posY, a.posZ, b.posX, b.posY, b.posZ );
+	}
+	
+	public static int getManhattanDistance( int ax, int ay, int az, ChunkCoordinates b )
+	{
+		return getManhattanDistance( ax, ay, az, b.posX, b.posY, b.posZ );
+	}
+	
+	public static int getManhattanDistance( ChunkCoordinates a, int bx, int by, int bz )
+	{
+		return getManhattanDistance( a.posX, a.posY, a.posZ, bx, by, bz );
+	}
+	
+	public static int getManhattanDistance( int ax, int ay, int az, int bx, int by, int bz )
+	{
+		return Math.abs( ax - bx ) + Math.abs( ay - by ) + Math.abs( az - bz );
+	}
+	
+	public static int getXZManhattanDistance( ChunkCoordinates a, ChunkCoordinates b )
+	{
+		return getXZManhattanDistance( a.posX, a.posY, a.posZ, b.posX, b.posY, b.posZ );
+	}
+	
+	public static int getXZManhattanDistance( int ax, int ay, int az,  ChunkCoordinates b )
+	{
+		return getXZManhattanDistance( ax, ay, az, b.posX, b.posY, b.posZ );
+	}
+	
+	public static int getXZManhattanDistance( ChunkCoordinates a, int bx, int by, int bz )
+	{
+		return getXZManhattanDistance( a.posX, a.posY, a.posZ, bx, by, bz );
+	}
+	
+	public static int getXZManhattanDistance( int ax, int ay, int az, int bx, int by, int bz )
+	{
+		return Math.abs( ax - bx ) + Math.abs( az - bz );
+	}
+	
+	public static List<ChunkCoordinates> graphSearch( World world, int x, int y, int z, int maxNumBlocks, BlockValidator validator )
+	{
+		ChunkCoordinates sourceBlock = new ChunkCoordinates( x, y, z );
+		
+		// grab all connected wood/leaf blocks with the same meta up to a few blocks away in the xz plane
+		// using BFS
+		ChunkCoordinates origin = new ChunkCoordinates( x, y, z );
+		LinkedHashSet<ChunkCoordinates> queue = new LinkedHashSet<ChunkCoordinates>();
+		HashSet<ChunkCoordinates> visitedBlocks = new HashSet<ChunkCoordinates>();
+		List<ChunkCoordinates> foundBlocks = new ArrayList<ChunkCoordinates>();
+		queue.add( origin );
+		
+		while( !queue.isEmpty() )
+		{
+			// get the block and visit it
+			ChunkCoordinates block = queue.iterator().next();
+			queue.remove( block );
+			visitedBlocks.add( block );
+			
+			if( !validator.isValid(  world, block.posX, block.posY, block.posZ ) )
+			{
+				continue;
+			}
+			
+			// this is a target block! Add it to the list (as long as it's not the source block)
+			if( !block.equals( sourceBlock ) )
+			{
+				// check for the fail-safe
+				if( foundBlocks.size() >= maxNumBlocks )
+				{
+					return null;
+				}
+				
+				foundBlocks.add( block );
+			}
+			
+			// queue up the block's neighbors
+			List<ChunkCoordinates> neighbors = new ArrayList<ChunkCoordinates>();
+			for( int dx : new int[] { -1, 1 } )
+			{
+				neighbors.add( new ChunkCoordinates( block.posX + dx, block.posY, block.posZ ) );
+			}
+			for( int dy : new int[] { -1, 1 } )
+			{
+				neighbors.add( new ChunkCoordinates( block.posX, block.posY + dy, block.posZ ) );
+			}
+			for( int dz : new int[] { -1, 1 } )
+			{
+				neighbors.add( new ChunkCoordinates( block.posX, block.posY, block.posZ + dz ) );
+			}
+			
+			for( ChunkCoordinates neighbor : neighbors )
+			{
+				if( !visitedBlocks.contains( neighbor ) && !queue.contains( neighbor ) )
+				{
+					queue.add( neighbor );
+				}
+			}
+		}
+		
+		return foundBlocks;
+	}
+}
---
+++ src/cuchaz/modsShared/ColorUtils.java
@@ -0,0 +1,36 @@
+package cuchaz.modsShared;
+
+import java.awt.Color;
+
+public class ColorUtils
+{
+	private static final int DefaultAlpha = 0;
+	
+	public static int getColor( Color c )
+	{
+		return getColor( c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha() );
+	}
+	
+	public static int getGrey( int grey )
+	{
+		return getColor( grey, grey, grey, DefaultAlpha );
+	}
+	
+	public static int getGrey( int grey, int a )
+	{
+		return getColor( grey, grey, grey, a );
+	}
+	
+	public static int getColor( int r, int g, int b )
+	{
+		return getColor( r, g, b, DefaultAlpha );
+	}
+	
+	public static int getColor( int r, int g, int b, int a )
+	{
+		return ( r & 0xff )
+			| ( g & 0xff ) << 8
+			| ( b & 0xff ) << 16
+			| ( a & 0xff ) << 24;
+	}
+}
