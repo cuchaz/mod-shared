e91cad76d2033d3b8c7d7552675be8473755c183
jeff <devnull@localhost>
2013-09-30 22:53:42 -0400

    added seconds/tick conversion utilities

--- src/cuchaz/modsShared/Util.java
+++ src/cuchaz/modsShared/Util.java
@@ -2,6 +2,23 @@ package cuchaz.modsShared;
 
 public class Util
 {
+	public static final int TicksPerSecond = 20; // constant set in Minecraft.java, inaccessible by methods
+	
+	public static double secondsToTicks( double x )
+	{
+		return x*TicksPerSecond;
+	}
+	
+	public static double ticksToSeconds( double x )
+	{
+		return x/TicksPerSecond;
+	}
+	
+	public static double perSecond2ToPerTick2( double x )
+	{
+		return x/TicksPerSecond/TicksPerSecond;
+	}
+	
 	public static int realModulus( int a, int b )
 	{
 		// NOTE: Java's % operator is not a true modulus
